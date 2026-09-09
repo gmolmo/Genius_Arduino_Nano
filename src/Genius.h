62b717907a8d340ab89067d194a448eab3644b11
gmolmo <guimesolmo@gmail.com>
2026-09-09 19:50:21 -0300

    Definicao do .h de teste inicial

---
+++ src/Genius.h
@@ -0,0 +1,21 @@
+#ifndef GENIUS_H
+#define GENIUS_H
+
+#include<Arduino.h>
+
+class Genius
+{
+    private:
+        const int* pinoLeds;
+        const int* pinoBotoes;
+        int pinoBuzzer;
+
+    public:
+        Genius(const int pinoLeds[], const int pinoBotoes[], int pinoBuzzer);
+
+        void iniciar();
+
+        void testar();
+};
+
+#endif
