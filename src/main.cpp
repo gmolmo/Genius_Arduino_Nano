ce34ade29bc80a6eadbf00600d5f0693e5396944
gmolmo <guimesolmo@gmail.com>
2026-09-15 19:18:06 -0300

    corrigido erro de reset da dificuldade

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,23 @@
 #include <Arduino.h>
 #include "Genius.h"
 
-const int pinoLeds[] = {5,6,10,11};
-const int pinobotoes[] = {2,3,8,12,A5};
-const int ledsdificuldade[] = {13, 9, 4};
-int buzzer = 7;
+const int pinoLeds[] = {6,5,4,3};
+const int pinobotoes[] = {A5,A4,A3,A2,A1};
+const int ledsdificuldade[] = {11,10,9};
+int buzzer = 12;
+
+int dificuldade;
 
 Genius jogo(pinoLeds,pinobotoes,buzzer,ledsdificuldade);
 
 void setup() {
     jogo.iniciar();
+    int dificuldade = 0;
 }
 
 void loop() {
 
   int nRodadas = 5;
-
-  int dificuldade = 0;
 
   dificuldade = jogo.standby();
 
